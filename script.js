acd866440a7d100519ad440aa08cb3f49c3dbe01
MohamedAly <MohamedAlyAhmed@Aol.com>
2022-09-15 01:09:31 +0200

    Add New Functions

--- script.js
+++ script.js
@@ -1,59 +1,85 @@
-
-const products = [
-    {
-        id: 0,
-        name: "iPhone x",
-        description: "asdasdasdASDASD asdas dasd sa",
-        price: 1000,
-        src: "imgs/f1.jpg"
-    },
-    {
-        id: 1,
-        name: "gelly",
-        description: "asdasdasdASDASD asdas dasd sa",
-        price: 1000,
-        src: "imgs/f2.jpg"
-    },
-    {
-        id: 2,
-        name: "BMW",
-        description: "asdasdasdASDASD asdas dasd sa",
-        price: 1000,
-        src: "imgs/f3.jpg"
-    },
-    {
-        id: 3,
-        name: "Marcides",
-        description: "asdasdasdASDASD asdas dasd sa",
-        price: 1000,
-        src: "imgs/f4.jpg"
-    },
-    {
-        id: 4,
-        name: "caddy",
-        description: "asdasdasdASDASD asdas dasd sa",
-        price: 1000,
-        src: "imgs/food.jpg"
-    }
-];
-
-var cardProducts = [];
-
+// Global Variables 
 
 var productsTemp = document.getElementById("products");
 var cardsTemp = document.getElementById("cards");
+var totalProducts = document.getElementById ("total");
+
+
+//JSON File For Products
+const products = [
+    {
+        id: 0,
+        name: "Anker Soundcore ",
+        description: "Gaming Headset, Stereo Sound , Sound Enhancement for FPS Games, Noise Isolating Mic",
+        price: 600,
+        src: "imgs/1.jpg"
+    },
+    {
+        id: 1,
+        name: "Bluetooth Speaker",
+        description: "Portable Wireless Bluetooth Speakers, Touch Control Bedside Table Light,Outdoor Speakers Bluetooth",
+        price: 117,
+        src: "imgs/2.jpg"
+    },
+    {
+        id: 2,
+        name: "REDRAGON K611",
+        description: "BES Gaming Mechanical TKL Keyboard - White Backlighting and Side RGB Light - Blue Switch - AR/EN key ",
+        price: 999,
+        src: "imgs/3.jpg"
+    },
+    {
+        id: 3,
+        name: "TL-WA850RE",
+        description: "Signal Rate: 11n: Up to 300Mbps dynamic, Good Quality with a high end, TP-Link TL-WA850RE Wi-Fi Range Extender",
+        price: 275,
+        src: "imgs/4.jpg"
+    },
+    {
+        id: 4,
+        name: "Meetion GM015",
+        description: "Lightweight Honeycomb RGB Gaming Mouse (6400 DPI) – For PC & Laptop – Black",
+        price: 170,
+        src: "imgs/5.jpg"
+    },
+    {
+        id: 5,
+        name: "Computer Stand",
+        description: "Choetech Foldable Computer Stand,Choetech Foldable Computer Stand",
+        price: 230,
+        src: "imgs/6.jpg"
+    },
+    {
+        id: 6,
+        name: "Mini PC",
+        description: "Windows 11 Pro Intel 4-Core J4125 128G M.2 SSD TRIGKEY N4 Working Micro PC Computer",
+        price: 6000,
+        src: "imgs/7.jpg"
+    },
+    {
+        id: 7,
+        name: "HP Smart Tank 516 ",
+        description: "High-Volume, Low-Cost Printing And Innovation Design- Get Up To 18,000 ",
+        price: 3000,
+        src: "imgs/8.jpg"
+    }
+];
+
+
+var cardProducts = [];
+
 
 function onloadPage() {
     var innerhtml = '';
     for (let i = 0; i < products.length; i++) {
         innerhtml += `
-        <div class="card col-6" style="width: 18rem;">
-        <img src="${products[i].src}" class="card-img-top" alt="...">
+        <div class="card col-4 bg-dark text-light border border-light flex-center-all m-1" style="width: 12rem; ">
+        <img src="${products[i].src}" class="card-img-top" alt="product">
         <div class="card-body">
-            <h5 class="card-title">${products[i].name}</h5>
+            <h6 class="card-title text-warning ">${products[i].name}</h6>
             <p class="card-text">${products[i].description}</p>
-                <p class="card-text">${products[i].price} $</p>
-            <a class="btn btn-primary" onclick="addToCard(${products[i].id})" >add to card</a>
+                <h6 class="card-text">Price : ${products[i].price} L.E</h6>
+            <a class="btn btn-warning" onclick="addToCard(${products[i].id})" >Add To Card</a>
         </div>
         </div>
         `;
@@ -64,7 +90,7 @@ function onloadPage() {
 }
 
 function reGetCard() {
-    // set initial card products valus.
+    // set initial card products values
     var cardResult = JSON.parse(localStorage.getItem("cardProducts"));
     if (cardResult != null) {
         cardProducts = cardResult;
@@ -73,7 +99,7 @@ function reGetCard() {
 }
 
 function getProductById(prID) {
-    // findd the product whose have the same id
+    // find the product whose have the same id
     for (let i = 0; i < products.length; i++) {
         if (products[i].id === prID) {
             return products[i];
@@ -108,28 +134,29 @@ function reBuildCardTemp() {
     var innerhtml = '';
     for (let i = 0; i < cardProducts.length; i++) {
         innerhtml += `
-        <div class="card mb-3" style="max-width: 540px;">
+        <div class="card mb-3 bg-dark text-light border-light" style="max-width: 540px;">
         <div class="row g-0">
-            <div class="col-md-4">
+            <div class="col-3">
                 <img src="${cardProducts[i].src}" class="img-fluid rounded-start" alt="...">
             </div>
-            <div class="col-md-8">
+            <div class="col-md-9">
                 <div class="card-body">
                 <div class="row">
                 <h5 class="col card-title">${cardProducts[i].name}</h5>
-                <button onclick="deleteFromCard(${cardProducts[i].id})" class="col-2">X</button>
+                <button  type="button" onclick="deleteFromCard(${cardProducts[i].id})" class="btn-close btn-close-white col-2" aria-label="Close"></button>
             </div>
-                    <div class="row">
-                        <div class="col-8">
-                            <span>count :</span>
-                            <button onclick="decreaseCount(${cardProducts[i].id})">-</button>
-                            <span>${cardProducts[i].count}</span>
-                            <button onclick="increaseCount(${cardProducts[i].id})">+</button>
-                        </div>
-                        <div class="col-4">
-                        ${cardProducts[i].price}$
-                        </div>
-                    </div>
+            <div class="row ">
+            <div class="col-8">
+                <span ">count : ${cardProducts[i].count} </span>
+                
+            </div>
+                <div class="col-12">Price:${cardProducts[i].price} L.E
+                </div>
+                <div class="row">
+                <button style="width:20%;" type="button" class=" btn btn-warning m-1" onclick="decreaseCount(${cardProducts[i].id})">-</button>
+                <button style="width:20%;" type="button" class=" btn btn-warning m-1" onclick="increaseCount(${cardProducts[i].id})">+</button>
+                </div>
+                </div>
                 </div>
             </div>
         </div>
@@ -139,25 +166,47 @@ function reBuildCardTemp() {
     // but it on innerHTML on cardTemp.
     cardsTemp.innerHTML = innerhtml;
     // Re Calcolate Total.
-    reCalcolateTotal();
+    reCalculateTotal();
     // ...
 }
 
-function reCalcolateTotal() {
-    // Mohamed Ali task
+
+function reCalculateTotal() {
+    // Mohamed Ali task (Done)
     // loop card products and add price to total var * count
+    var total = 0;
+    for (var i = 0; i < cardProducts.length; i++) {
+        total += cardProducts[i].price;
+    }
+    totalProducts.innerHTML = total+" L.E";
 }
+
+
+
 
 function deleteFromCard(prID) {
     // Mohamed Ali task
     // delete from list.
-    for (let i = 0; i < cardProducts.length; i++) {
-        // find the product and splice it.
+    for (let index = 0; index < cardProducts.length; index++) {
+        if(cardProducts[index]["id"] == prID){
+            cardProducts.splice(index,1);
+        }
+        
     }
+
+localStorage.cardProducts = JSON.stringify(cardProducts);
     // delete from localStorage
     saveCardProducts();
     reBuildCardTemp();
 }
+
+
+function deleteAllProducts() {
+localStorage.clear();
+location.reload();
+}
+
+
 
 function increaseCount(prID) {
     // abo Zeaid tyyyyyyiz task
